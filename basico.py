c15b4c39cce2bb40c18bec5503d99d94858bfea0
rvfx04 <rvfx04@gmail.com>
2024-07-07 00:19:07 -0500

    Create basico.py

---
+++ basico.py
@@ -0,0 +1,35 @@
+# Importar las librerias necesarias
+import streamlit as st
+import pandas as pd
+import pyodbc
+from datetime import datetime, timedelta
+# Función para conectarse a BD y ejecutar una consulta
+def execute_query(query):
+ conn = pyodbc.connect(
+ "DRIVER={ODBC Driver 17 for SQL Server};"
+ "SERVER=" + st.secrets["server"] + ";"
+ "DATABASE=" + st.secrets["database"] + ";"
+ "UID=" + st.secrets["username"] + ";"
+ "PWD=" + st.secrets["password"] + ";"
+ )
+ df = pd.read_sql(query, conn)
+ conn.close()
+ return df
+
+# Título de la aplicación
+st.title("Título") 
+# Consulta SQL
+query = f"""
+ select
+ IdmaeAnexo_Cliente,
+ NommaeAnexoCliente
+ from maeAnexoCliente
+ """
+# Ejecutar la consulta
+df = execute_query(query)
+# Artificio para quitar la columna que numera las filas
+df = df.set_index(df.columns[0])
+# Mostrar el número de registros
+st.write(f"Número de registros: {len(df)}")
+# Mostrar el resultado en formato de tabla
+st.dataframe(df) 
