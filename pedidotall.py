326aae880d78570925b3642ec2da1835fb01e532
rvfx04 <rvfx04@gmail.com>
2024-07-26 12:03:21 -0500

    Update pedidotall.py

--- pedidotall.py
+++ pedidotall.py
@@ -42,7 +42,6 @@ def load_data(pedido, estilos):
     WHERE 
         b.coddocordenventa = ?
         {}
-
     """.format("AND f.nommaeestilo IN ({})".format(', '.join('?' * len(estilos))) if estilos else "")
 
     params = [pedido] + estilos if estilos else [pedido]
@@ -53,30 +52,41 @@ def load_data(pedido, estilos):
 
     return df
 
-# Sidebar para seleccionar el pedido
-pedido = st.sidebar.selectbox("Selecciona el pedido", options=[413979, 413980, 413981]) # debés cambiar estos valores por tus pedidos reales
+# Sidebar para ingresar el pedido
+pedido_input = st.sidebar.text_input("Ingresa el pedido (ejemplo: 413979)", "")
 
-# Sidebar para seleccionar estilos
-if pedido:
+estilos_seleccionados = []
+if pedido_input:
+    # Obtener estilos disponibles para el pedido
     with get_connection() as conn:
-        estilos_query = "SELECT DISTINCT nommaeestilo FROM maeestilo f INNER JOIN docordenventaitem a ON f.idmaeestilo = a.idmaeestilo WHERE a.iddocumento_ordenventa = ?"
-        estilos = pd.read_sql(estilos_query, conn, params=[pedido])['nommaeestilo'].tolist()
+        estilos_query = """
+        SELECT DISTINCT f.nommaeestilo 
+        FROM maeestilo f 
+        INNER JOIN docordenventaitem a ON f.idmaeestilo = a.idmaeestilo 
+        INNER JOIN docordenventa b ON a.iddocumento_ordenventa = b.iddocumento_ordenventa 
+        WHERE b.coddocordenventa = ?
+        """
+        estilos = pd.read_sql(estilos_query, conn, params=[pedido_input])['nommaeestilo'].tolist()
 
+    # Sidebar para seleccionar estilos
     estilos_seleccionados = st.sidebar.multiselect("Selecciona estilos", opciones=estilos)
 
 # Cargar los datos
 if st.sidebar.button("Actualizar"):
-    df = load_data(pedido, estilos_seleccionados)
+    if pedido_input:
+        df = load_data(pedido_input, estilos_seleccionados)
 
-    # Creación de tabla dinámica
-    if not df.empty:
-        pivot_table = pd.pivot_table(
-            df,
-            values='cant',
-            index=['estilo', 'cliente'],      # Eje vertical
-            columns=['combo', 'talla'],        # Eje horizontal
-            fill_value=0
-        )
-        st.write(pivot_table)
+        # Creación de tabla dinámica
+        if not df.empty:
+            pivot_table = pd.pivot_table(
+                df,
+                values='cant',
+                index=['estilo', 'cliente'],      # Eje vertical
+                columns=['combo', 'talla'],        # Eje horizontal
+                fill_value=0
+            )
+            st.write(pivot_table)
+        else:
+            st.write("No hay resultados para la consulta.")
     else:
-        st.write("No hay resultados para la consulta.")
+        st.warning("Por favor, ingresa un número de pedido.")
