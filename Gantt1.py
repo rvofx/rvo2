c5dd1e24baf6fb4ec318616b7f063afda78d54f1
rvfx04 <rvfx04@gmail.com>
2024-09-09 11:55:23 -0500

    Update Gantt1.py

--- Gantt1.py
+++ Gantt1.py
@@ -16,7 +16,234 @@ def get_data(pedido):
 
     query = f"""
         select * from (
-            -- Aquí va la consulta SQL proporcionada por ti
+            (SELECT
+    a.CoddocOrdenVenta AS PEDIDO, 
+    a.IdDocumento_OrdenVenta,
+    CASE WHEN ISDATE(a.dtFechaEmision) = 1 THEN CONVERT(DATE, a.dtFechaEmision) ELSE NULL END AS F_EMISION,
+    CASE WHEN ISDATE(a.dtFechaEntrega) = 1 THEN CONVERT(DATE, a.dtFechaEntrega) ELSE NULL END AS F_ENTREGA,
+    CONVERT(INT, a.dtFechaEntrega - a.dtFechaEmision) AS DIAS,
+    SUBSTRING(b.NommaeAnexoCliente, 1, 15) AS CLIENTE,
+    a.nvDocumentoReferencia AS PO,
+    CONVERT(INT, COALESCE(d.KG, 0)) AS KG_REQ,
+    --CONVERT(INT, COALESCE(t.KG_ARM, 0)) AS KG_ARM,
+    FORMAT(CASE WHEN d.KG = 0 THEN 0 ELSE (COALESCE(t.KG_ARM, 0) / d.KG) END, '0%') AS KG_ARMP,
+    --CONVERT(INT, COALESCE(t.KG_TEÑIDOS, 0)) AS KG_TEÑIDOS,
+    FORMAT(CASE WHEN d.KG = 0 THEN 0 ELSE (COALESCE(t.KG_TEÑIDOS, 0) / d.KG) END, '0%') AS KG_TENIDP,
+    --CONVERT(INT, COALESCE(t.KG_PRODUC, 0)) AS KG_DESPACH,
+    FORMAT(CASE WHEN d.KG = 0 THEN 0 ELSE (COALESCE(t.KG_PRODUC, 0) / d.KG) END, '0%') AS KG_TELAPROBP,
+    CONVERT(INT, a.dCantidad) AS UNID,
+    --CONVERT(INT, COALESCE(programado.PROG, 0)) AS PROG,
+    FORMAT(CASE WHEN a.dCantidad = 0 THEN 0 ELSE (COALESCE(programado.PROG, 0) / a.dCantidad) END, '0%') AS PROGP,
+    --CONVERT(INT, COALESCE(cortado.CORTADO, 0)) AS CORTADO,
+    FORMAT(CASE WHEN a.dCantidad = 0 THEN 0 ELSE (COALESCE(cortado.CORTADO, 0) / a.dCantidad) END, '0%') AS CORTADOP,
+    --CONVERT(INT, COALESCE(cosido.COSIDO, 0)) AS COSIDO,
+    FORMAT(CASE WHEN a.dCantidad = 0 THEN 0 ELSE (COALESCE(cosido.COSIDO, 0) / a.dCantidad) END, '0%') AS COSIDOP
+FROM docOrdenVenta a
+INNER JOIN maeAnexoCliente b ON a.IdmaeAnexo_Cliente = b.IdmaeAnexo_Cliente
+LEFT JOIN (
+    SELECT
+        c.IdDocumento_Referencia AS PEDIDO,
+        SUM(c.dCantidad) AS KG
+    FROM docOrdenVentaItem c
+    WHERE c.IdDocumento_Referencia > 0
+    GROUP BY c.IdDocumento_Referencia
+) d ON a.IdDocumento_OrdenVenta = d.PEDIDO
+LEFT JOIN (
+    SELECT
+        x.IdDocumento_Referencia AS PEDIDO,
+        SUM(y.dCantidadProgramado) AS KG_ARM,
+        SUM(z.bcerrado * y.dCantidadRequerido) AS KG_PRODUC,
+        SUM(s.bcerrado * y.dCantidadProgramado) AS KG_TEÑIDOS
+    FROM docOrdenProduccionItem y
+    INNER JOIN docOrdenProduccion z ON y.IdDocumento_OrdenProduccion = z.IdDocumento_OrdenProduccion
+    INNER JOIN docOrdenVentaItem x ON (z.IdDocumento_Referencia = x.IdDocumento_OrdenVenta AND y.idmaeItem = x.IdmaeItem)
+    INNER JOIN docOrdenProduccionRuta s ON y.IdDocumento_OrdenProduccion = s.IdDocumento_OrdenProduccion
+    WHERE s.IdmaeReceta > 0
+    GROUP BY x.IdDocumento_Referencia
+) t ON a.IdDocumento_OrdenVenta = t.PEDIDO
+LEFT JOIN (
+    SELECT 
+        g.IdDocumento_OrdenVenta,
+        SUM(a.dCantidadProgramado) AS PROG
+    FROM dbo.docOrdenProduccion c WITH (NOLOCK)
+    INNER JOIN dbo.docOrdenProduccionItem a WITH (NOLOCK)
+        ON c.IdDocumento_OrdenProduccion = a.IdDocumento_OrdenProduccion
+    INNER JOIN dbo.docOrdenVenta g WITH (NOLOCK)
+        ON c.IdDocumento_Referencia = g.IdDocumento_OrdenVenta
+    INNER JOIN dbo.docOrdenProduccionRuta b WITH (NOLOCK)
+        ON c.IdDocumento_OrdenProduccion = b.IdDocumento_OrdenProduccion
+    WHERE c.bCerrado = 0
+        AND c.bAnulado = 0
+        AND c.IdtdDocumentoForm = 127
+        AND b.IdmaeCentroCosto = 29
+    GROUP BY g.IdDocumento_OrdenVenta
+) AS programado
+ON a.IdDocumento_OrdenVenta = programado.IdDocumento_OrdenVenta
+LEFT JOIN (
+    SELECT 
+        g.IdDocumento_OrdenVenta,
+        SUM(b.dCantidadIng) AS CORTADO
+    FROM dbo.docNotaInventario a WITH (NOLOCK)
+    INNER JOIN dbo.maeCentroCosto a1 WITH (NOLOCK)
+        ON a.IdmaeCentroCosto = a1.IdmaeCentroCosto
+        AND a1.bConOrdenProduccion = 1
+    INNER JOIN dbo.docNotaInventarioItem b WITH (NOLOCK)
+        ON a.IdDocumento_NotaInventario = b.IdDocumento_NotaInventario
+    INNER JOIN dbo.docOrdenProduccion c WITH (NOLOCK)
+        ON a.IdDocumento_OrdenProduccion = c.IdDocumento_OrdenProduccion
+    INNER JOIN dbo.docOrdenVenta g WITH (NOLOCK)
+        ON c.IdDocumento_Referencia = g.IdDocumento_OrdenVenta
+    WHERE a.IdtdDocumentoForm = 131
+        AND a.bDevolucion = 0
+        AND a.bDesactivado = 0
+        AND a.bAnulado = 0
+        AND a.IdmaeCentroCosto = 29
+    GROUP BY g.IdDocumento_OrdenVenta
+) AS cortado
+ON a.IdDocumento_OrdenVenta = cortado.IdDocumento_OrdenVenta
+LEFT JOIN (
+    SELECT 
+        g.IdDocumento_OrdenVenta,
+        SUM(b.dCantidadIng) AS COSIDO
+    FROM dbo.docNotaInventario a WITH (NOLOCK)
+    INNER JOIN dbo.maeCentroCosto a1 WITH (NOLOCK)
+        ON a.IdmaeCentroCosto = a1.IdmaeCentroCosto
+        AND a1.bConOrdenProduccion = 1
+    INNER JOIN dbo.docNotaInventarioItem b WITH (NOLOCK)
+        ON a.IdDocumento_NotaInventario = b.IdDocumento_NotaInventario
+    INNER JOIN dbo.docOrdenProduccion c WITH (NOLOCK)
+        ON a.IdDocumento_OrdenProduccion = c.IdDocumento_OrdenProduccion
+    INNER JOIN dbo.docOrdenVenta g WITH (NOLOCK)
+        ON c.IdDocumento_Referencia = g.IdDocumento_OrdenVenta
+    WHERE a.IdtdDocumentoForm = 131
+        AND a.bDevolucion = 0
+        AND a.bDesactivado = 0
+        AND a.bAnulado = 0
+        AND a.IdmaeCentroCosto = 47
+    GROUP BY g.IdDocumento_OrdenVenta
+) AS cosido
+ON a.IdDocumento_OrdenVenta = cosido.IdDocumento_OrdenVenta
+WHERE
+    a.IdtdDocumentoForm = 10
+    AND a.IdtdTipoVenta = 4
+    AND a.bAnulado = 0
+    AND (CASE WHEN ISDATE(a.dtFechaEntrega) = 1 THEN CONVERT(DATE, a.dtFechaEntrega) ELSE NULL END) BETWEEN '2024-08-01' AND '2024-12-31' ) gg
+
+INNER JOIN (SELECT 
+    x.IdDocumento_OrdenVenta,
+    --x.CoddocOrdenVenta,
+	q0.FMINARM,
+	q0.FMAXARM,
+    q1.FMINTENID,
+    q1.FMAXTENID,
+    q2.FMINTELAPROB,
+    q2.FMAXTELAPROB,
+    q3.FMINCORTE,
+    q3.FMAXCORTE,
+    q4.FMINCOSIDO,
+    q4.FMAXCOSIDO
+FROM docOrdenVenta x
+LEFT JOIN (
+    SELECT 
+        x.IdDocumento_OrdenVenta, 
+        MIN(b.dtFechaEmision) AS FMINARM,
+		MAX(b.dtFechaEmision) AS FMAXARM
+    FROM docOrdenVentaItem a
+    INNER JOIN docOrdenProduccion b ON b.IdDocumento_Referencia = a.IdDocumento_OrdenVenta
+    INNER JOIN docOrdenVenta x ON a.IdDocumento_Referencia = x.IdDocumento_OrdenVenta
+   
+    WHERE b.IdtdDocumentoForm = 138 
+      AND b.IdtdDocumentoForm_Referencia = 152 
+      AND x.CoddocOrdenVenta IS NOT NULL
+      AND a.IdDocumento_Referencia > 0
+    GROUP BY x.IdDocumento_OrdenVenta
+) q0 ON x.IdDocumento_OrdenVenta = q0.IdDocumento_OrdenVenta
+
+LEFT JOIN (
+    SELECT 
+        x.IdDocumento_OrdenVenta, 
+        MIN(e.dtFechaHoraFin) AS FMINTENID,
+        MAX(e.dtFechaHoraFin) AS FMAXTENID
+    FROM docOrdenVentaItem a
+    INNER JOIN docOrdenProduccion b ON b.IdDocumento_Referencia = a.IdDocumento_OrdenVenta
+    INNER JOIN docOrdenVenta x ON a.IdDocumento_Referencia = x.IdDocumento_OrdenVenta
+    INNER JOIN docRecetaOrdenProduccion d ON b.IdDocumento_OrdenProduccion = d.IdDocumento_OrdenProduccion
+    INNER JOIN docReceta e ON d.IdDocumento_Receta = e.IdDocumento_Receta
+    WHERE b.IdtdDocumentoForm = 138 
+      AND b.IdtdDocumentoForm_Referencia = 152 
+      AND x.CoddocOrdenVenta IS NOT NULL
+      AND a.IdDocumento_Referencia > 0
+    GROUP BY x.IdDocumento_OrdenVenta
+) q1 ON x.IdDocumento_OrdenVenta = q1.IdDocumento_OrdenVenta
+LEFT JOIN (
+    SELECT 
+        x.IdDocumento_OrdenVenta,  
+        MIN(b.FechaCierreAprobado) AS FMINTELAPROB,
+        MAX(b.FechaCierreAprobado) AS FMAXTELAPROB
+    FROM docOrdenVentaItem a
+    INNER JOIN docOrdenProduccion b ON b.IdDocumento_Referencia = a.IdDocumento_OrdenVenta
+    INNER JOIN docOrdenVenta x ON a.IdDocumento_Referencia = x.IdDocumento_OrdenVenta
+    INNER JOIN docOrdenProduccionRuta d ON b.IdDocumento_OrdenProduccion = d.IdDocumento_OrdenProduccion
+    WHERE b.IdtdDocumentoForm = 138 
+      AND b.IdtdDocumentoForm_Referencia = 152 
+      AND x.CoddocOrdenVenta IS NOT NULL
+      AND a.IdDocumento_Referencia > 0
+    GROUP BY x.IdDocumento_OrdenVenta
+) q2 ON x.IdDocumento_OrdenVenta = q2.IdDocumento_OrdenVenta
+LEFT JOIN (
+    SELECT 
+        g.IdDocumento_OrdenVenta,  
+        MIN(a.dtFechaRegistro) AS FMINCORTE,
+        MAX(a.dtFechaRegistro) AS FMAXCORTE
+    FROM dbo.docNotaInventario a WITH (NOLOCK)
+    INNER JOIN dbo.maeCentroCosto a1 WITH (NOLOCK) ON a.IdmaeCentroCosto = a1.IdmaeCentroCosto AND a1.bConOrdenProduccion = 1
+    INNER JOIN dbo.docNotaInventarioItem b WITH (NOLOCK) ON a.IdDocumento_NotaInventario = b.IdDocumento_NotaInventario AND b.dCantidadIng <> 0
+    INNER JOIN dbo.docOrdenProduccion c WITH (NOLOCK) ON a.IdDocumento_OrdenProduccion = c.IdDocumento_OrdenProduccion 
+	--AND c.bCerrado = 0 
+	AND c.bAnulado = 0 AND c.IdtdDocumentoForm = 127
+    INNER JOIN dbo.docOrdenVenta g WITH (NOLOCK) ON c.IdDocumento_Referencia = g.IdDocumento_OrdenVenta
+    INNER JOIN dbo.docOrdenProduccionRuta d WITH (NOLOCK) ON a.IddocOrdenProduccionRuta = d.IddocOrdenProduccionRuta
+    INNER JOIN dbo.docOrdenProduccionItem e WITH (NOLOCK) ON c.IdDocumento_OrdenProduccion = e.IdDocumento_OrdenProduccion AND b.IdmaeItem_Inventario = e.IdmaeItem
+    INNER JOIN dbo.maeItemInventario f WITH (NOLOCK) ON b.IdmaeItem_Inventario = f.IdmaeItem_Inventario AND f.IdtdItemForm = 10
+    WHERE a.IdtdDocumentoForm = 131
+        AND a.bDevolucion = 0
+        AND a.bDesactivado = 0
+        AND a.bAnulado = 0
+        AND a.IdDocumento_OrdenProduccion <> 0
+        AND a.IdmaeCentroCosto = 29
+    GROUP BY g.IdDocumento_OrdenVenta
+) q3 ON x.IdDocumento_OrdenVenta = q3.IdDocumento_OrdenVenta
+LEFT JOIN (
+    SELECT 
+        g.IdDocumento_OrdenVenta,  
+        MIN(a.dtFechaRegistro) AS FMINCOSIDO,
+        MAX(a.dtFechaRegistro) AS FMAXCOSIDO
+    FROM dbo.docNotaInventario a WITH (NOLOCK)
+    INNER JOIN dbo.maeCentroCosto a1 WITH (NOLOCK) ON a.IdmaeCentroCosto = a1.IdmaeCentroCosto AND a1.bConOrdenProduccion = 1
+    INNER JOIN dbo.docNotaInventarioItem b WITH (NOLOCK) ON a.IdDocumento_NotaInventario = b.IdDocumento_NotaInventario AND b.dCantidadIng <> 0
+    INNER JOIN dbo.docOrdenProduccion c WITH (NOLOCK) ON a.IdDocumento_OrdenProduccion = c.IdDocumento_OrdenProduccion 
+	--AND c.bCerrado = 0 
+	AND c.bAnulado = 0 AND c.IdtdDocumentoForm = 127
+    INNER JOIN dbo.docOrdenVenta g WITH (NOLOCK) ON c.IdDocumento_Referencia = g.IdDocumento_OrdenVenta
+    INNER JOIN dbo.docOrdenProduccionRuta d WITH (NOLOCK) ON a.IddocOrdenProduccionRuta = d.IddocOrdenProduccionRuta
+    INNER JOIN dbo.docOrdenProduccionItem e WITH (NOLOCK) ON c.IdDocumento_OrdenProduccion = e.IdDocumento_OrdenProduccion AND b.IdmaeItem_Inventario = e.IdmaeItem
+    INNER JOIN dbo.maeItemInventario f WITH (NOLOCK) ON b.IdmaeItem_Inventario = f.IdmaeItem_Inventario AND f.IdtdItemForm = 10
+    WHERE a.IdtdDocumentoForm = 131
+        AND a.bDevolucion = 0
+        AND a.bDesactivado = 0
+        AND a.bAnulado = 0
+        AND a.IdDocumento_OrdenProduccion <> 0
+        AND a.IdmaeCentroCosto = 47
+    GROUP BY g.IdDocumento_OrdenVenta
+) q4 ON x.IdDocumento_OrdenVenta = q4.IdDocumento_OrdenVenta
+WHERE x.CoddocOrdenVenta IS NOT NULL
+		and x.IdtdDocumentoForm=10 
+		and x.IdtdTipoVenta=4
+		and x.bAnulado=0
+		--and c.IdDocumento_OrdenVenta=441563
+--ORDER BY x.IdDocumento_OrdenVenta; 
+) ff  
+ON gg.IdDocumento_OrdenVenta = ff.IdDocumento_OrdenVenta
             ) WHERE PEDIDO = '{pedido}'
         """
     data = pd.read_sql(query, conn)
