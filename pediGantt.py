e5eeccb76bc8e21e85a37517060da5efcb7c5ce7
rvfx04 <rvfx04@gmail.com>
2024-09-10 16:53:32 -0500

    Update pediGantt.py

--- pediGantt.py
+++ pediGantt.py
@@ -28,7 +28,7 @@ FROM
     CASE WHEN ISDATE(a.dtFechaEmision) = 1 THEN CONVERT(DATE, a.dtFechaEmision) ELSE NULL END AS F_EMISION,
     CASE WHEN ISDATE(a.dtFechaEntrega) = 1 THEN CONVERT(DATE, a.dtFechaEntrega) ELSE NULL END AS F_ENTREGA,
     DATEDIFF(DAY, a.dtFechaEmision, a.dtFechaEntrega) AS DIAS,
-    --CONVERT(INT, a.dtFechaEntrega - a.dtFechaEmision) AS DIAS,
+   
     SUBSTRING(b.NommaeAnexoCliente, 1, 15) AS CLIENTE,
     a.nvDocumentoReferencia AS PO,
     CONVERT(INT, COALESCE(d.KG, 0)) AS KG_REQ,
