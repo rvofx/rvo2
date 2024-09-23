1e9f99c965d2889d9ee7f9f343fd63cd7c1d9b25
rvfx04 <rvfx04@gmail.com>
2024-09-23 15:03:48 -0500

    Update pruebagantt.py

--- pruebagantt.py
+++ pruebagantt.py
@@ -331,8 +331,6 @@ if st.button("Ejecutar Consulta"):
                 # Recalcular DIAS si ambas fechas de colocación y entrega han sido ingresadas
                 if fecha_colocacion_input is not None and fecha_entrega_input is not None:
                     dias = (pd.to_datetime(fecha_entrega_input) - pd.to_datetime(fecha_colocacion_input)).days
-	st.write({dias})
-
 
                 # Cálculo de las fechas de inicio y fin
                 start_armado = f_emision + timedelta(days=FACTOR * (dias-dtex-dpieza-dprenda))
