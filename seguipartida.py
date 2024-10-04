c6148afe427a01aec75b6971330f34c22b8a9ba9
rvfx04 <rvfx04@gmail.com>
2024-10-04 07:33:39 -0500

    Update seguipartida.py

--- seguipartida.py
+++ seguipartida.py
@@ -52,10 +52,12 @@ def get_partidas_con_tenido_sin_aprob_tela(dias):
     conn = connect_to_db()
     query = f"""
         SELECT a.CoddocOrdenProduccion AS PARTIDA,DATEDIFF(DAY, a.dtFechaEmision, GETDATE()) AS DIAS    , DATEDIFF(DAY, j.dtFechaHoraFin, GETDATE()) AS DIAS_TEN  , LEFT(f.NommaeItemInventario, 35) AS TELA, FORMAT(a.dtFechaEmision, 'dd-MM') AS F_EMISION,
-              FORMAT(j.dtFechaHoraFin, 'dd-MM') AS F_TENIDO, FORMAT(a.FechaCierreAprobado, 'dd-MM') AS F_APROB_TELA, a.dCantidad AS KG, 
-               a.nvDocumentoReferencia AS REF, g.NommaeColor AS COLOR, a.bCierreAprobado AS AP_DES, 
-               a.bProduccionAprobado AS DESP, a.bcerrado AS CERR, LEFT(h.NommaeAnexoCliente, 15) AS Cliente, 
-               a.ntEstado AS ESTADO, k.NommaeRuta AS RUTA
+              FORMAT(j.dtFechaHoraFin, 'dd-MM') AS F_TENIDO, --FORMAT(a.FechaCierreAprobado, 'dd-MM') AS F_APROB_TELA, 
+              a.dCantidad AS KG, 
+               a.nvDocumentoReferencia AS REF, g.NommaeColor AS COLOR, --a.bCierreAprobado AS AP_DES, 
+               --a.bProduccionAprobado AS DESP, --a.bcerrado AS CERR, 
+               LEFT(h.NommaeAnexoCliente, 15) AS Cliente
+               --a.ntEstado AS ESTADO, k.NommaeRuta AS RUTA
         FROM docOrdenProduccion a WITH (NOLOCK)
         INNER JOIN maeItemInventario f WITH (NOLOCK) ON f.IdmaeItem_Inventario = a.IdmaeItem
         INNER JOIN maeColor g WITH (NOLOCK) ON g.IdmaeColor = a.IdmaeColor
