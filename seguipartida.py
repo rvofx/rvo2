96181c8e58d489222435d9f0a2fddf76e03b227d
rvfx04 <rvfx04@gmail.com>
2024-10-04 12:31:58 -0500

    Update seguipartida.py

--- seguipartida.py
+++ seguipartida.py
@@ -27,7 +27,7 @@ def get_partidas_sin_tenido(dias):
                --a.bProduccionAprobado AS DESP, a.bcerrado AS CERR, 
                LEFT(h.NommaeAnexoCliente, 15) AS Cliente, 
                --a.ntEstado AS ESTADO, k.NommaeRuta AS RUTA,
-               CASE WHEN LOWER(k.NommaeRuta) LIKE '%mofijado%' THEN 1 ELSE 0 END AS MOFIJADO_FLAG
+               --CASE WHEN LOWER(k.NommaeRuta) LIKE '%mofijado%' THEN 1 ELSE 0 END AS MOFIJADO_FLAG
         FROM docOrdenProduccion a WITH (NOLOCK)
         INNER JOIN maeItemInventario f WITH (NOLOCK) ON f.IdmaeItem_Inventario = a.IdmaeItem
         INNER JOIN maeColor g WITH (NOLOCK) ON g.IdmaeColor = a.IdmaeColor
