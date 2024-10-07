4740bd2b422fa1e4d383c51cc6c3210255b97b86
rvfx04 <rvfx04@gmail.com>
2024-10-07 11:45:00 -0500

    Update seguipartida.py

--- seguipartida.py
+++ seguipartida.py
@@ -174,7 +174,7 @@ if st.button("Mostrar partidas no TEÑIDAS"):
     styled_df = df_sin_tenido.style.apply(highlight_mofijado, axis=1)
     # Aplicar formato con un decimal a la columna KG
     styled_df = df_sin_tenido.style.apply(highlight_mofijado, axis=1).format({"KG": "{:.1f}"})
-    st.write(styled_df.hide(axis='index'), unsafe_allow_html=True)  # Aquí ocultamos el índice
+    st.write(df_sin_tenido.to_html(index=False), unsafe_allow_html=True)
     #st.write(styled_df, unsafe_allow_html=True)
     
 
