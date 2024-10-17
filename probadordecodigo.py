e05ecd5013936d9c382b4b6b5e5c58c4b507b39d
rvfx04 <rvfx04@gmail.com>
2024-10-17 16:07:47 -0500

    Update probadordecodigo.py

--- probadordecodigo.py
+++ probadordecodigo.py
@@ -101,7 +101,7 @@ df_detalle = df_detalle.rename(columns={
 df_detalle = df_detalle[['OP', 'FECHA_ENVIO', 'FECHA_RETORNO', 'PROVEEDOR', 'ENVIADO', 'RETORNADO', 'SALDO']]
 
 # Mostrar resultados en Streamlit
-st.title("Análisis de Órdenes de Producción")
+st.title("Confeccion OP, 47 B")
 
 st.header("Resumen por Proveedor")
 st.dataframe(df_resumen)
