467959d2c4b43660db82a52dee199275ee11faa3
rvfx04 <rvfx04@gmail.com>
2024-10-29 15:39:27 -0500

    Update probadordecodigo.py

--- probadordecodigo.py
+++ probadordecodigo.py
@@ -337,9 +337,11 @@ if st.button("Ejecutar Consulta"):
                 # Fechas de colocación y entrega
                 
 		#fecha_colocacion = datetime(2024,6,24)
-		fecha_colocacion = datetime(2024,6,24)  
+		#fecha_colocacion = datetime(2024,6,24)  
 		#fecha_colocacion = pd.to_datetime(df['F_EMISION'].iloc[0])   
                 fecha_entrega = pd.to_datetime(df['F_ENTREGA'].iloc[0])
+	    fecha_colocacion = datetime(2024,6,24) 
+	    
 
                 # Agregar líneas verticales para las fechas de colocación y entrega
                 fig.add_shape(
