cccb80b1c94befe8ab5c4a6a137ac00644cf6f66
rvfx04 <rvfx04@gmail.com>
2024-10-29 15:56:41 -0500

    Update probadordecodigo.py

--- probadordecodigo.py
+++ probadordecodigo.py
@@ -348,7 +348,7 @@ if st.button("Ejecutar Consulta"):
                     x1=fecha_colocacion,
                     y1=len(df_gantt),
                     line=dict(color="green", width=2, dash="dash"),
-                    name="Fecha Colocación"
+                    name="Fecha Colocación", hovertext=f"Fecha Colocación: {fecha_colocacion.strftime('%Y-%m-%d')}"
                 )
                 fig.add_shape(
                     type="line",
